b61418f26835b75d15d4f208c408df04bffdef24
Kayo <kayoanderson0403@gmail.com>
2018-05-02 22:28:19 -0300

    Registration validation, some progress with Vue and Vuetify and populating the database

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,6 +4,8 @@ module.exports = (sequelize, DataTypes) => {
   		type: DataTypes.STRING,
   		unique: true
   	},
-  	password: DataTypes.STRING
+  	password: {
+  		type: DataTypes.STRING
+  	}
   })
 }
