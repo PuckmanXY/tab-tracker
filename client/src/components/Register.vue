50ef672d59694ce4a80f85d87285a3856f909208
Kayo <kayoanderson0403@gmail.com>
2018-05-03 00:19:26 -0300

    /register route is materialy beautiful

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-layout column>
+  <v-layout columns>
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar flat dense class="cyan">
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
-      </div>
-      <div class="pl-4 pr-4 pt-2 pb-2">
-        <input
-          type="email"
-          name="email"
-          v-model="email"
-          placeholder="email" />
-        <br>
-        <input
-          type="password"
-          name="password"
-          v-model="password"
-          placeholder="password" />
-        <br>
-        <div class="error" v-html="error"></div>
-        <v-btn class="cyan" @click="register">Register</v-btn>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          >
+          </v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            v-model="password"
+          >
+          </v-text-field>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
       </div>
     </v-flex>
   </v-layout>
